c48fef1c68278ed7147e66365777b796ae3a78d6
Kalos Lazo <lzm.kalos@gmail.com>
2024-03-09 13:37:22 -0500

    feature: add function for handle links dynamically

--- app/js/getUserData.js
+++ app/js/getUserData.js
@@ -2,11 +2,22 @@
 // GITHUB: @kaloslazo
 
 const USERNAME_ELEMENT = document.getElementById("username_data");
+const LINKS_GRID = document.getElementById("links_grid");
 
-const getCurrentUsername = async () => {
-  const dbUser = await fetch("db/userData.json").then((res) => res.json());
+const setupUserData = async () => {
+  const userData = await fetch("db/userData.json").then((res) => res.json());
+  const userNickname = userData.nickname;
+  const userSocialLinks = userData.socialLinks;
 
-  USERNAME_ELEMENT.textContent = dbUser.username;
+  USERNAME_ELEMENT.textContent = userNickname;
+  userSocialLinks.map((eachLink) => {
+    let currentLink = eachLink;
+
+    return (LINKS_GRID.innerHTML += `
+        <a href="${currentLink.url}" target="_blank" class="inline-flex items-center justify-center transition-colors ease-linear border-2 border-transparent rounded-lg hover:border-ctp-blue bg-ctp-base aspect-square hover:bg-ctp-surface0/50">
+          <span class="${currentLink.iconClass} text-6xl" alt="Icon ${currentLink.url}"></span>
+        </a>`);
+  });
 };
 
-getCurrentUsername();
+setupUserData();
